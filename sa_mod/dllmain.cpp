e7ba58f0f4cd3891d616a38760dc39b80d70773b
LemonHaze <guizhang@wulinshu.com>
2021-12-19 22:35:39 +0000

    Modified sample code

--- sa_mod/dllmain.cpp
+++ sa_mod/dllmain.cpp
@@ -6,6 +6,7 @@
 //#define NO_LOG_IN_DEBUG                   // Disables logging in Debug
 
 #include "SDK.h"
+#include "MinHook.h"
 
 using namespace CG;
 
@@ -13,35 +14,66 @@ bool IsNameDefault(std::string name) {
     return name.find("Default") != std::string::npos;
 }
 
-static bool gThread = false;
-static bool bFlatRadar = false;
+static bool             gThread     = false;
+static bool             bFlatRadar  = false;
 
-static AGTAGameMode* gGameMode = nullptr;
-static UGameterface* gInterface = nullptr;
-static AGTARadar* gRadar = nullptr;
+static AGTAGameMode*    gGameMode   = nullptr;
+static UGameterface*    gInterface  = nullptr;
+static AGTARadar*       gRadar      = nullptr;
+
+static void CheckGlobals()
+{
+    if (!gThread)
+        return;
+
+    if (!gGameMode) {
+        for (const auto& gameMode : UObject::FindObjects<AGTAGameMode>())
+            gGameMode = gameMode;
+    }
+    if (gGameMode && !gInterface) {
+        if (gGameMode->GetGameterface())
+            gInterface = gGameMode->GetGameterface();
+    }
+    if (!gGameMode && !gInterface) {
+        for (const auto& interface : UObject::FindObjects<UGameterface>())
+            gInterface = interface;
+    }
+    if (gInterface && !gRadar) {
+        if (gInterface->Radar)
+            gRadar = gInterface->Radar;
+    }
+    if (!gRadar) {
+        for (const auto& tmp : UObject::FindObjects<AGTARadar>())
+            gRadar = tmp;
+    }
+}
 
 static void Thread()
 {
     LOG("Thread started!");
 
-    Sleep(18 * 1000);   // 18sec. delay to ensure the engine is brought up before we run our code.
+    GameDetails details = GetGameDetails();
+    if (details.version == eVersion::INVALID) {
+        LOG("Couldn't find offsets.");
+        return;
+    }
+
+    Sleep(30 * 1000);   // 18sec. delay to ensure the engine is brought up before we run our code.
+
+    if (!InitSdk(details)) {
+        LOG("SDK couldn't be initialized!");
+    } else {
+        LOG("SDK initialized");
+        gThread = true;
+    }
 
     while (gThread) {
-        if (!gGameMode) {
-            for (const auto& gameMode : UObject::FindObjects<AGTAGameMode>())
-                gGameMode = gameMode;
-        } 
-        if (gGameMode && !gInterface) {
-            if (gGameMode->GetGameterface())
-                gInterface = gGameMode->GetGameterface();
-        }
-        if (gInterface && !gRadar) {
-            if (gInterface->Radar)
-                gRadar = gInterface->Radar;
-        } 
-        if (!gRadar) {
-            for (const auto& tmp : UObject::FindObjects<AGTARadar>())
-                gRadar = tmp;
+        CheckGlobals();
+
+        if (gInterface) {
+            LOG("%s", gInterface->IsPlayingGame() ? "Playing game" : "Not playing game");
+            //if (gInterface->CurrentMenu)
+                //LOG("Current Menu: %s", gInterface->CurrentMenu->Name.GetName().c_str());
         }
 
         if (gRadar) {
@@ -66,7 +98,8 @@ void Attach()
 {
     LOG("Attached!");
 
-    Sleep(2 * 1000);   // 2sec. delay to ensure the engine is brought up before we run our code.
+    // @todo: Implement hook on StartIntroMovie()
+    // 140B9F710 - SA - Gameterface::StartIntroMovie
 
 #   if defined(_DEBUG) || defined(CONSOLE_IN_RELEASE)
         AllocConsole();
@@ -74,18 +107,10 @@ void Attach()
         freopen("CONOUT$", "w", stdout);
         freopen("CONOUT$", "w", stderr);
         SetConsoleTitleA(MOD_STRING);
-        LOG("Allocated console\n");
+        LOG("Allocated console");
 #   endif
 
-    if (!InitSdk()) {
-        LOG("SDK couldn't be initialized!");
-        gThread = false; 
-        return;
-    } else {
-        LOG("SDK initialized");
-        gThread = true;
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&Thread, NULL, 0, NULL);
-    }
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&Thread, NULL, 0, NULL);
 }
 void Detach() 
 {
